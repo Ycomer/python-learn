693ad6532ceb1c911c0e0efdb3a4efaf679b7437
Ycomer <dongfangyuan126@126.com>
2017-04-30 10:55:26 +0800

    delete wrong message

--- item_list3/resume.js
+++
@@ -1,4 +0,0 @@
-var formattedRole = HTMLheaderRole.replace("%data%", 陕西西安);
-var formattedName = HTMLheaderName.replace("data%", 董方园);
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
